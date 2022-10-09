71692d28b3421108a8b5cc9910dd9e0cbbd27feb
Scorpion197 <kamelgaouaoui197@gmail.com>
2022-10-09 15:03:17 +0100

    added an endpoint for re-confirming email

--- backend/ecommerce/ecommerce/urls.py
+++ backend/ecommerce/ecommerce/urls.py
@@ -18,5 +18,12 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("ecommerce_app.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path(
+        "",
+        include("ecommerce_app.urls"),
+    ),
+    path("auth/registration/", include("dj_rest_auth.registration.urls")),
+]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
